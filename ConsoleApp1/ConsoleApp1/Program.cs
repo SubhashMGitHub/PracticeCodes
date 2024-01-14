dd853302c964b41e66db895078112f7121687113
Subhash <msubhash535@gmail.com>
2024-01-14 13:12:35 -0600

    comminting and pushing the changes to repo.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Test
     {
+        public virtual void Display()
+        {
+            Console.WriteLine("This is a virtual method!!!");
+        }
+    }
+    class Program:Test
+    {
+        public override void Display()
+        {
+            Console.WriteLine("This is a override method!!!");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world...");
+            Console.WriteLine("Run time polymorphism example...");
+            Program obj = new Program();
+            obj.Display();
             Console.ReadKey();
         }
     }
